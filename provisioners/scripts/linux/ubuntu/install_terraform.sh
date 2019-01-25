9c9a5f72c61ca92fed284d08e8695e93f6d5dc84
james woolfenden <james.woolfenden@slalom.com>
2019-01-25 11:25:36 +0000

    fix terraform version

--- provisioners/scripts/linux/ubuntu/install_terraform.sh
+++ provisioners/scripts/linux/ubuntu/install_terraform.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-cd /tmp/ 
+cd /tmp/
 VERSION="0.11.11"
 sudo apt install -y zip unzip
-wget https://releases.hashicorp.com/terraform/{$VERSION}/terraform_{$VERSION}_linux_amd64.zip
-unzip terraform_{$VERSION}_linux_amd64.zip
+wget "https://releases.hashicorp.com/terraform/$VERSION/terraform_${VERSION}_linux_amd64.zip"
+unzip "terraform_${VERSION}_linux_amd64.zip"
 sudo mv /tmp/terraform /usr/local/bin/
